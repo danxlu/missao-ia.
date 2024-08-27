a445f8b6628a2739a2264c201192cbce03dbc90a
danxlu <148556538+danxlu@users.noreply.github.com>
2024-08-27 10:56:24 -0300

    ddd

---
+++ main.js
@@ -0,0 +1,20 @@
+const caixaPrincipal = document.querySelector(".caixa-principal"); 
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixalternativas");
+const caixaResultado = document.querySelector(".caixa-resultado");
+const caixaTexto = document.querySelector(".texto-resultado");
+
+let Pergunta = "Qual é o seu nome ?";
+
+let Perguntas = [ "qual é o seu nome", "qual sua idade", "qual sua altura"];
+
+let PerguntaseRespostas = [
+    {
+        enunciado: "Qual é o seu nome ?"
+        alternativas: ["alternativa1","alternativa2","alternativa3"]
+    }
+
+
+
+
+]
